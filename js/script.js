687659ac8cf9744c68497745157c03b5b2c7c136
AndreaMatarrese <matarreseandrea47@gmail.com>
2022-11-08 18:49:33 +0100

    exercise without control

--- js/script.js
+++ js/script.js
@@ -10,3 +10,56 @@
 4--creo il prompt per far inserire i numeri all'utente (prompt 5 numeri--push in nuovo arra)
 5--controllo finale tra i numeri inseriti e quelli stampati in precedenza(controllo primo array vs secondo array con condizione)
 */
+
+const container = document.querySelector('.container')
+const numbers = document.querySelector('.numbers')
+const numbersEnd = document.querySelector('.numbers-end')
+const btnStart = document. querySelector('#start')
+const btnVerify = document.querySelector('#verify')
+
+const arrNumStart = [];
+
+const arrNumEnd = [];
+// comparsa numeri al click del bottone
+btnStart.addEventListener('click', function(){
+    for(let i=0; i<5; i++){
+        btnStart.classList.add('hide');
+        const numStart = parseInt(Math.random() * 100);
+        console.log('Numeri', numStart);
+        arrNumStart.push(numStart);
+    }
+    numbers.append(arrNumStart);
+    // funzione per la disattivazione
+    setTimeout(visualizzazione, 3000);
+    function visualizzazione(){
+        numbers.classList.add('hide');
+        btnStart.classList.add('hide');
+    }
+    // funzione per l'inserimento dati
+    setTimeout(insert, 4000)
+    function insert(){
+        for(let i=0; i<5; i++){
+            numbersEnd.innerHTML='';
+            const numEnd = parseInt(prompt('inserisci i 5 numeri'));
+            arrNumEnd.push(numEnd);
+            numbersEnd.append(arrNumEnd);
+        }
+        console.log('Numeri inseriti', arrNumEnd)
+    }
+});
+
+    /*controllo dei dati inseriti con quelli originali 
+    se (il nuovo array include numeri del vecchio array, stampa i numeri indovinati)
+    altrimenti (stampa: nessun numero uguale a quelli stampati)*/
+   
+btnVerify.addEventListener('click', function(){
+    let numStart =
+        if(NumEnd=== arrNumStart[i]){
+            console.log('hai una buona memoria')
+        } else{
+            console.log('non hai una buona memoria')
+        }
+    
+});  
+
+
