13500facfd1f7258533a634d657cb5c18b4079db
AndreaMatarrese <matarreseandrea47@gmail.com>
2022-11-08 16:57:13 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Descrizione:
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+/*
+1--creo numeri casuali e li metto in una variabile (mathrandom-for-push)
+2--appendo i numeri casuali creati al container(append all'array)
+3--imposto il timer di 30 secondi e faccio scomparire i numeri(timer timer 30 secondi--hide dei numeri)
+4--creo il prompt per far inserire i numeri all'utente (prompt 5 numeri--push in nuovo arra)
+5--controllo finale tra i numeri inseriti e quelli stampati in precedenza(controllo primo array vs secondo array con condizione)
+*/
